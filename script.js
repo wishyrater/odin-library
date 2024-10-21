1fc9c73db12a824bbd0287f8ab5d278088a338a3
Julian Andersen <julian.cesar.andersen@gmail.com>
2024-10-22 01:30:00 +0200

    Reset script

--- script.js
+++ script.js
@@ -1,56 +1,75 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", () => {
     const myLibrary = [];
 
     function Book(title, author, pages) {
         this.title = title;
         this.author = author;
         this.pages = pages;
-    }
-    
-    function addBookToLibrary(book) {
-        myLibrary.push(book);
-    }
-    
+    };
+
+    const addBookToLibrary = (book) => myLibrary.push(book);
+
+    const tableBody = document.querySelector("tbody");
+
+    function addBooksToTable() {
+        tableBody.innerHTML = '';
+        myLibrary.forEach((book) => {
+            const tableRow = document.createElement("tr");
+            for (let key in book) {
+                const tableData = document.createElement("td");
+                tableData.textContent = book[key];
+                tableRow.appendChild(tableData);
+            }
+            const removeCell = document.createElement("td");
+            const removeButton = document.createElement("button");
+            removeButton.textContent = 'Remove';
+            removeButton.setAttribute("data-index", myLibrary.indexOf(book));
+            removeButton.addEventListener("click", (e) => {
+                const index = e.target.getAttribute('data-index');
+                myLibrary.splice(index, 1);
+                addBooksToTable(myLibrary);
+            })
+            removeCell.appendChild(removeButton);
+            tableRow.appendChild(removeCell);
+            tableBody.appendChild(tableRow);
+        });
+    };
+
     const harryPotter = new Book("Harry Potter", "J.K. Rowling", 295);
-    const gameOfThrones = new Book("A Clash of Kings", "George R.R. Martin", 473);
-    
+    const gameOfThrones = new Book("A Clash of Kings", "George R.R. Martin", 348);
+
     addBookToLibrary(harryPotter);
     addBookToLibrary(gameOfThrones);
-    
-    const tableBody = document.querySelector("#library tbody");
 
-    function addBookToTable(book) {
-        const tableRow = document.createElement("tr");
-        for (key in book) {
-            const tableData = document.createElement("td");
-            tableData.textContent = book[key];
-            tableRow.appendChild(tableData);
-        }
-        tableBody.appendChild(tableRow);
-    }
-    
-    myLibrary.forEach((book) => addBookToTable(book));
+    addBooksToTable(myLibrary);
 
-    const showButton = document.querySelector("#show-dialog-button");
-    const dialog = document.querySelector("dialog");
+    const dialog = document.querySelector("#dialog");
+    const showDialogButton = document.querySelector("#show-dialog-button");
     const confirmButton = document.querySelector("#confirm");
-    const titleEl = document.querySelector("#title");
-    const authorEl = document.querySelector("#author");
-    const pagesEl = document.querySelector("#pages");
-
-    showButton.addEventListener("click", () => {
+    const titleInput = document.querySelector("#title");
+    const authorInput = document.querySelector("#author");
+    const pagesInput = document.querySelector("#pages");
+    
+    showDialogButton.addEventListener("click", () => {
         dialog.showModal();
-    });
-
-    dialog.addEventListener("close", () => {
-        if (dialog.returnValue === "default" && (titleEl.value && authorEl.value && pagesEl.value)) {
-            let newBook = new Book(titleEl.value, authorEl.value, pagesEl.value);
-            addBookToTable(newBook);
-        }
     });
 
     confirmButton.addEventListener("click", (e) => {
         e.preventDefault();
-        dialog.close(confirmButton.value);
+        dialog.close(e.target.value);
     });
-});
+
+    dialog.addEventListener("close", (e) => {
+        if (dialog.returnValue === "default"){
+            const newBook = new Book(titleInput.value, authorInput.value, pagesInput.value);
+            addBookToLibrary(newBook);
+            addBooksToTable(myLibrary);
+        }
+
+        titleInput.value = '';
+        authorInput.value = '';
+        pagesInput.value = '';
+    });
+
+
+})
