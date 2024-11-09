e9a145c09ab97c385c495fe2b508dce63ab50f10
Julian Andersen <julian.cesar.andersen@gmail.com>
2024-11-09 01:28:44 +0100

    Create displaycontroller class with init

--- script.js
+++ script.js
@@ -1,140 +1,130 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const myLibrary = [];
-
-    class Book {
-        constructor(title, author, pages, read) {
-            this.title = title;
-            this.author = author;
-            this.pages = pages;
-            this.read = read;
-        }
-
-        toggleRead() {
-            this.read = !this.read;
-        }
-
-        addBookToLibrary(library) {
-            library.push(this);
-        }
-
-        getProperties() {
-            return [this.title, this.author, this.pages, this.read];
-        }
+class Library {
+    constructor() {
+        this.library = [];
     }
 
-    class Table {
-        constructor(name) {
-            this._name = name;
-        }
-
-        renderTable(library) {
-            const tableBody = document.querySelector("tbody");
-            tableBody.innerHTML = '';
-            library.forEach((book) => {
-                // create the book info
-                const tableRow = document.createElement("tr");
-                for (let property of book.getProperties()) {
-                    const tableData = document.createElement("td");
-                    tableData.textContent = property;
-                    tableRow.appendChild(tableData);
-                }
-
-                // create button for toggling read
-                const toggleReadCell = document.createElement("td");
-                const toggleReadButton = document.createElement("button");
-                toggleReadButton.textContent = 'Toggle read';
-                toggleReadButton.setAttribute("data-index", library.indexOf(book));
-                toggleReadButton.setAttribute("data-action", "toggle-read");
-                // TO DO: handle event listener in display class
-                toggleReadCell.appendChild(toggleReadButton);
-                tableRow.appendChild(toggleReadCell);
-
-                // create remove row button
-                const removeCell = document.createElement("td");
-                const removeButton = document.createElement("button");
-                removeButton.textContent = 'Remove';
-                removeButton.setAttribute("data-index", library.indexOf(book));
-                removeButton.setAttribute("data-action", "remove");
-                removeCell.appendChild(removeButton);
-                tableRow.appendChild(removeCell);
-
-                tableBody.appendChild(tableRow);
-            })
-        }
+    addBookToLibrary(book) {
+        this.library.push(book);
     }
 
-    function addBooksToTable() {
+    getLibrary() {
+        return this.library;
+    }
+
+    renderTable() {
+        const tableBody = document.querySelector("tbody");
         tableBody.innerHTML = '';
-        myLibrary.forEach((book) => {
+        this.library.forEach((book) => {
+            // create the book info
             const tableRow = document.createElement("tr");
-            for (let key in book) {
-                if (typeof book[key] !== 'function') {
-                    const tableData = document.createElement("td");
-                    tableData.textContent = book[key];
-                    tableRow.appendChild(tableData);
-                }
+            for (let property of book.getProperties()) {
+                const tableData = document.createElement("td");
+                tableData.textContent = property;
+                tableRow.appendChild(tableData);
             }
+
+            // create button for toggling read
             const toggleReadCell = document.createElement("td");
             const toggleReadButton = document.createElement("button");
             toggleReadButton.textContent = 'Toggle read';
-            toggleReadButton.setAttribute("data-index", myLibrary.indexOf(book));
-            toggleReadButton.addEventListener("click", (e) => {
-                const index = e.target.getAttribute("data-index");
-                myLibrary.at(index).toggleRead();
-                addBooksToTable(myLibrary);
-            });
+            toggleReadButton.setAttribute("data-index", this.library.indexOf(book));
+            toggleReadButton.setAttribute("data-action", "toggle-read");
             toggleReadCell.appendChild(toggleReadButton);
             tableRow.appendChild(toggleReadCell);
+
+            // create remove row button
             const removeCell = document.createElement("td");
             const removeButton = document.createElement("button");
             removeButton.textContent = 'Remove';
-            removeButton.setAttribute("data-index", myLibrary.indexOf(book));
-            removeButton.addEventListener("click", (e) => {
-                const index = e.target.getAttribute('data-index');
-                myLibrary.splice(index, 1);
-                addBooksToTable(myLibrary);
-            })
+            removeButton.setAttribute("data-index", this.library.indexOf(book));
+            removeButton.setAttribute("data-action", "remove");
             removeCell.appendChild(removeButton);
             tableRow.appendChild(removeCell);
+
             tableBody.appendChild(tableRow);
         });
-    };
+    }
+}
 
-    const harryPotter = new Book("Harry Potter", "J.K. Rowling", 295, true);
-    const gameOfThrones = new Book("A Clash of Kings", "George R.R. Martin", 348, false);
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
 
-    addBookToLibrary(harryPotter);
-    addBookToLibrary(gameOfThrones);
+    toggleRead() {
+        this.read = !this.read;
+    }
 
-    addBooksToTable(myLibrary);
+    getProperties() {
+        return [this.title, this.author, this.pages, this.read];
+    }
+}
 
-    const dialog = document.querySelector("#dialog");
-    const showDialogButton = document.querySelector("#show-dialog-button");
-    const confirmButton = document.querySelector("#confirm");
-    const titleInput = document.querySelector("#title");
-    const authorInput = document.querySelector("#author");
-    const pagesInput = document.querySelector("#pages");
-    
-    showDialogButton.addEventListener("click", () => {
-        dialog.showModal();
-    });
+class DisplayController {
+    constructor() {
+        this.library = new Library();
+        this.dialog = document.querySelector("#dialog");
+        this.showDialogButton = document.querySelector("#show-dialog-button");
+        this.confirmButton = document.querySelector("#confirm");
+        this.titleInput = document.querySelector("#title");
+        this.authorInput = document.querySelector("#author");
+        this.pagesInput = document.querySelector("#pages");
+        this.table = document.querySelector("table");
 
-    confirmButton.addEventListener("click", (e) => {
-        e.preventDefault();
-        dialog.close(e.target.value);
-    });
+        this.init();
+    }
 
-    dialog.addEventListener("close", (e) => {
-        if (dialog.returnValue === "default"){
-            const newBook = new Book(titleInput.value, authorInput.value, pagesInput.value);
-            addBookToLibrary(newBook);
-            addBooksToTable(myLibrary);
-        }
+    init() {
+        const harryPotter = new Book("Harry Potter", "J.K. Rowling", 295, true);
+        const gameOfThrones = new Book("A Clash of Kings", "George R.R. Martin", 348, false);
 
-        titleInput.value = '';
-        authorInput.value = '';
-        pagesInput.value = '';
-    });
+        this.library.addBookToLibrary(harryPotter);
+        this.library.addBookToLibrary(gameOfThrones);
 
+        this.addEventListeners();
+        this.library.renderTable();
+    }
 
+    addEventListeners() {
+        this.showDialogButton.addEventListener("click", () => {
+            this.dialog.showModal();
+        });
+
+        this.confirmButton.addEventListener("click", (e) => {
+            e.preventDefault();
+            this.dialog.close(e.target.value);
+        });
+
+        this.dialog.addEventListener("close", () => {
+            if (dialog.returnValue === "default") {
+                const newBook = new Book(this.titleInput.value, 
+                    this.authorInput.value, 
+                    parseInt(this.pagesInput.value), 
+                    false);
+                this.library.addBookToLibrary(newBook);
+                this.library.renderTable();
+            }
+
+            this.titleInput.value = '';
+            this.authorInput.value = '';
+            this.pagesInput.value = '';
+        });
+
+        this.table.addEventListener("click", (e) => {
+            if (e.target.getAttribute("data-action") === 'toggle-read') {
+                this.library.getLibrary().at(e.target.getAttribute("data-index")).toggleRead();
+                this.library.renderTable();
+            } else if (e.target.getAttribute("data-action") === 'remove') {
+                this.library.getLibrary().splice(e.target.getAttribute("data-index"), 1);
+                this.library.renderTable();
+            }
+        });
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    const displayController = new DisplayController();
 })
